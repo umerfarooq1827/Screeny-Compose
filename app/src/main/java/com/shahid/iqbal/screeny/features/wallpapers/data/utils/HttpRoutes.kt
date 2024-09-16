9e2836b71eaff9e4d870d57a8ceb77a3a76a25a7
shahidzbi4213 <shahid.iqbal4213@gmail.com>
2024-09-16 17:24:29 +0500

    Api Setup

--- app/src/main/java/com/shahid/iqbal/screeny/features/wallpapers/data/utils/HttpRoutes.kt
+++ app/src/main/java/com/shahid/iqbal/screeny/features/wallpapers/data/utils/HttpRoutes.kt
@@ -2,6 +2,6 @@ package com.shahid.iqbal.screeny.features.wallpapers.data.utils
 
 object HttpRoutes {
 
-    const val BASE_URL = "https://api.pexels.com/v1/"
-    const val GET_WALLPAPERS = "curated"
+    private const val BASE_URL = "https://api.pexels.com/v1/"
+    const val GET_WALLPAPERS = "${BASE_URL}/curated"
 }
